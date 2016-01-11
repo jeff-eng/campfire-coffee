22296f18122f7896707e57e6062410eecf2e8abf
Jeff Eng <jeff@jeffeng.me>
2016-01-11 14:03:58 -0800

    Created objects for locations

---
+++ js/app.js
@@ -0,0 +1,41 @@
+var pikePlace = {
+  minCustPerHr: 14,
+  maxCustPerHr: 55,
+  cupsPerCust: 1.2,
+  lbsPerCust: 3.7
+};
+
+var capitolHill = {
+  minCustPerHr: 32,
+  maxCustPerHr: 48,
+  cupsPerCust: 3.2,
+  lbsPerCust: 0.4
+};
+
+var seaPubLib = {
+  minCustPerHr: 49,
+  maxCustPerHr: 75,
+  cupsPerCust:  2.6,
+  lbsPerCust: 0.2
+}
+
+var sLakeUnion = {
+  minCustPerHr: 35,
+  maxCustPerHr: 88,
+  cupsPerCust: 1.3,
+  lbsPerCust: 3.7
+}
+
+var seaTacAirport = {
+  minCustPerHr: 68,
+  maxCustPerHr: 124,
+  cupsPerCust: 1.1,
+  lbsPerCust: 2.7
+}
+
+var websiteSales = {
+  minCustPerHr: 3,
+  maxCustPerHr: 6,
+  cupsPerCust: 0,
+  lbsPerCust: 6.7
+}
