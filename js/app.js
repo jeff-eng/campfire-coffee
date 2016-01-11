81659c790a46cebfffc35bce5e0ff08552d73eb7
Jeff Eng <jeff@jeffeng.me>
2016-01-11 14:41:44 -0800

    Added a method for hourly customers and linked javascript file to the HTML

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Location as objects
 var pikePlace = {
   minCustPerHr: 14,
   maxCustPerHr: 55,
@@ -17,25 +18,33 @@ var seaPubLib = {
   maxCustPerHr: 75,
   cupsPerCust:  2.6,
   lbsPerCust: 0.2
-}
+};
 
 var sLakeUnion = {
   minCustPerHr: 35,
   maxCustPerHr: 88,
   cupsPerCust: 1.3,
   lbsPerCust: 3.7
-}
+};
 
 var seaTacAirport = {
   minCustPerHr: 68,
   maxCustPerHr: 124,
   cupsPerCust: 1.1,
   lbsPerCust: 2.7
-}
+};
 
 var websiteSales = {
   minCustPerHr: 3,
   maxCustPerHr: 6,
   cupsPerCust: 0,
   lbsPerCust: 6.7
+};
+
+//need a method to calculate the number of customers per hour
+var hourlyCustomers = function(minCustPerHr, maxCustPerHr) {
+  return math.floor(math.random() * maxCustPerHr - minCustPerHr + 1) + minCustPerHr;
+  console.log(hourlyCustomers);
 }
+
+hourlyCustomers(68, 124);
