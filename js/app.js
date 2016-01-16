cc40bdee90b6093b9612162d238a3cfcedf98e36
Jeff Eng <jeff@jeffeng.me>
2016-01-15 17:07:43 -0800

    Formatted tables and data

--- js/app.js
+++ js/app.js
@@ -31,6 +31,143 @@ Kiosk.prototype.populateArrays = function() {
         }
        console.log(this.customerPerHrArray);
      }
+
+//Create instances
+var pikePlace = new Kiosk('Pike Place Market', 14, 55, 1.2, 3.7);
+var capitolHill = new Kiosk('Capitol Hill', 32, 48, 3.2, 0.4);
+var seaPubLib = new Kiosk('Seattle Public Library', 49, 75, 2.6, 0.2);
+var sLakeUnion = new Kiosk('South Lake Union', 35, 88, 1.3, 3.7);
+var seaTacAirport = new Kiosk('Sea-Tac Airport', 68, 124, 1.1, 2.7);
+var websiteSales= new Kiosk('Website Sales', 3, 6, 0, 6.7);
+
+//Create function to render data into table format
+function renderTable(location) {
+  //Create first row with location and each hour
+  var tableEl = document.createElement('table'); //Create table element
+  document.body.appendChild(tableEl);  //Append table element to DOM
+  var trEl = document.createElement('tr'); //Create table row element
+  tableEl.appendChild(trEl);  //Append table row element to table element
+
+
+  var thEl = document.createElement('th');  //Create empty table heading
+  trEl.appendChild(thEl);  //Append table data to table row
+  thEl.textContent = location.name;
+
+  for(var i = 0; i < hours.length; i++) {  //Loop to create table headings for the hours
+
+    var thEl = document.createElement('th'); //Create table heading
+    trEl.appendChild(thEl);      //Append table heading to DOM
+    thEl.textContent = hours[i];  //Give the table heading text content
+  }
+
+  location.populateArrays();
+
+    var thEl = document.createElement('th');  //Create empty table heading
+    var newTr = document.createElement('tr');
+    tableEl.appendChild(newTr);
+    newTr.appendChild(thEl);  //Append table data to table row
+    thEl.textContent = '[Customers]';
+
+    for(var i = 0; i < location.customerPerHrArray.length; i++) {
+
+      var tdEl = document.createElement('td');
+      tdEl.textContent = location.customerPerHrArray[i];
+      newTr.appendChild(tdEl);
+    }
+//**************
+    var thEl = document.createElement('th');  //Create empty table heading
+    var newTr = document.createElement('tr');
+    tableEl.appendChild(newTr);
+    newTr.appendChild(thEl);  //Append table data to table row
+    thEl.textContent = '[Cups Sold (lbs)]';
+
+    for(var i = 0; i < location.customerPerHrArray.length; i++) {  //Loop to create table headings for the hours
+
+      var tdEl = document.createElement('td');
+      tdEl.textContent = Math.ceil(location.numCupsSoldArray[i]) + '(' + location.cupsToLbsArray[i].toFixed(1) + ')';
+      newTr.appendChild(tdEl);
+    }
+//***************
+    var thEl = document.createElement('th');  //Create empty table heading
+    var newTr = document.createElement('tr');
+    tableEl.appendChild(newTr);
+    newTr.appendChild(thEl);  //Append table data to table row
+    thEl.textContent = '[To-Go lbs]';
+
+    for(var i = 0; i < location.customerPerHrArray.length; i++) {  //Loop to create table headings for the hours
+
+      var tdEl = document.createElement('td');
+      tdEl.textContent = location.toGoLbsArray[i].toFixed(1);
+      newTr.appendChild(tdEl);
+    }
+//*****************
+    var thEl = document.createElement('th');  //Create empty table heading
+    var newTr = document.createElement('tr');
+    tableEl.appendChild(newTr);
+    newTr.appendChild(thEl);  //Append table data to table row
+    thEl.textContent = '[Total lbs]';
+
+    for(var i = 0; i < location.customerPerHrArray.length; i++) {  //Loop to create table headings for the hours
+
+      var tdEl = document.createElement('td');
+      tdEl.textContent = location.totalLbsSoldPerHrArray[i].toFixed(1);
+      newTr.appendChild(tdEl);
+    }
+//*****************
+    var thEl = document.createElement('th'); //Creates table heading
+    var newTr = document.createElement('tr'); //Creates table row
+    tableEl.appendChild(newTr);  //Appends table row to the table
+    newTr.appendChild(thEl);  //Appends table heading to the table row
+    thEl.textContent = '[Daily Total lbs]:';
+
+    var tdEl = document.createElement('td');
+    newTr.appendChild(tdEl);
+    tdEl.textContent = this.totalDailyLbs;
+
+    var brEl = document.createElement('br');  //Create line break
+    document.body.appendChild(brEl);
+
+}
+var allKiosks = [pikePlace, capitolHill, seaPubLib, sLakeUnion, seaTacAirport, websiteSales];
+
+function renderTables() {
+  for (var i=0; i<allKiosks.length; i++){
+    renderTable(allKiosks[i]);
+  }
+}
+
+renderTables();
+
+var newKiosk = document.getElementById('newKiosk');
+newKiosk.addEventListener('submit', addNewKiosk);
+
+function addNewKiosk(event) {
+  console.log(event);
+  event.preventDefault();
+
+  var storeName = event.target.storeName.value;
+  var minCust = event.target.minCust.value;
+  var maxCust = event.target.maxCust.value;
+  var avgCups = event.target.avgCups.value;
+  var avgPnds = event.target.avgPnds.value;
+
+  var eventKiosk = new Kiosk(storeName, minCust, maxCust, avgCups, avgPnds);
+  renderTable(eventKiosk);
+}
+
+
+//Aggregate Kiosk Sales
+// var aggregateKioskSales = 0; //Initialize to zero
+
+// var kiosks = [this.pikePlace, this.capitolHill, this.seaPubLib, this.sLakeUnion, this.seaTacAirport, this.websiteSales]; //Array of kiosk objects
+// for (var i = 0; i < kiosks.length; i++) {
+//   kiosks[i].render();
+//   aggregateKioskSales += kiosks[i].totalDailyLbs;
+// }
+// var h1El = document.createElement('h1');
+// h1El.textContent = 'Total daily pounds of coffee for all locations is: ' + aggregateKioskSales.toFixed(1) + ' lbs.';
+// document.body.appendChild(h1El);
+
 //**numCupsSold Method**
 // Kiosk.prototype.numCupsSold = function() {
 //       for (var i = 0; i < hours.length; i++) {
@@ -74,129 +211,16 @@ Kiosk.prototype.populateArrays = function() {
 //       this.totalLbsSoldPerHr();
 //       this.totalDailyLbsMethod();
 
-    //   var ulEl = document.createElement('ul');
-    //   ulEl.appendChild(document.createTextNode(this.name));
-    //   for(var i = 0; i < hours.length; i++) {
-    //     var liEl = document.createElement('li');
-    //     //6:00am: 86.5 lbs [23 customers, 27.6 cups (1.4 lbs), 85.1 lbs to-go]
-    //     liEl.textContent = hours[i] + ': ' + this.totalLbsSoldPerHrArray[i].toFixed(1) + ' lbs [' + this.customerPerHrArray[i] + ' customers, ' + this.numCupsSoldArray[i].toFixed(1) + ' cups (' + this.cupsToLbsArray[i].toFixed(1) + ' lbs), ' + this.toGoLbsArray[i].toFixed(1) + ' lbs to-go]';
-    //     ulEl.appendChild(liEl);
-    //   }
-    //   document.body.appendChild(ulEl); //This takes the <ul> element and puts it in the body as child element
-    //   liEl = document.createElement('li');
-    //   liEl.textContent = 'TOTAL Daily Pounds At ' + this.name + ': ' + this.totalDailyLbs.toFixed(1) + ' lbs';
-    //   ulEl.appendChild(liEl);
-    // }
-
-//Create instances
-var pikePlace = new Kiosk('Pike Place Market', 14, 55, 1.2, 3.7);
-var capitolHill = new Kiosk('Capitol Hill', 32, 48, 3.2, 0.4);
-var seaPubLib = new Kiosk('Seattle Public Library', 49, 75, 2.6, 0.2);
-var sLakeUnion = new Kiosk('South Lake Union', 35, 88, 1.3, 3.7);
-var seaTacAirport = new Kiosk('Sea-Tac Airport', 68, 124, 1.1, 2.7);
-var websiteSales= new Kiosk('Website Sales', 3, 6, 0, 6.7);
-
-//Create function to render data into table format
-function renderTable(location) {
-  //Create first row with location and each hour
-  var tableEl = document.createElement('table'); //Create table element
-  document.body.appendChild(tableEl);  //Append table element to DOM
-  var trEl = document.createElement('tr'); //Create table row element
-  tableEl.appendChild(trEl);  //Append table row element to table element
-
-
-  var thEl = document.createElement('th');  //Create empty table heading
-  trEl.appendChild(thEl);  //Append table data to table row
-  thEl.textContent = location.name;
-
-  for(var i = 0; i < hours.length; i++) {  //Loop to create table headings for the hours
-
-    var thEl = document.createElement('th'); //Create table heading
-    trEl.appendChild(thEl);      //Append table heading to DOM
-    thEl.textContent = hours[i];  //Give the table heading text content
-  }
-
-  location.populateArrays();
-
-    var thEl = document.createElement('th');  //Create empty table heading
-    var newTr = document.createElement('tr');
-    tableEl.appendChild(newTr);
-    newTr.appendChild(thEl);  //Append table data to table row
-    thEl.textContent = 'customers';
-
-    for(var i = 0; i < location.customerPerHrArray.length; i++) {
-
-      var tdEl = document.createElement('td');
-      tdEl.textContent = location.customerPerHrArray[i];
-      newTr.appendChild(tdEl);
-    }
-//**************
-    var thEl = document.createElement('th');  //Create empty table heading
-    var newTr = document.createElement('tr');
-    tableEl.appendChild(newTr);
-    newTr.appendChild(thEl);  //Append table data to table row
-    thEl.textContent = 'Cups Sold';
-
-
-    for(var i = 0; i < location.customerPerHrArray.length; i++) {  //Loop to create table headings for the hours
-
-      var tdEl = document.createElement('td');
-      tdEl.textContent = Math.ceil(location.numCupsSoldArray[i]);
-      newTr.appendChild(tdEl);
-    }
-//***************
-    var thEl = document.createElement('th');  //Create empty table heading
-    var newTr = document.createElement('tr');
-    tableEl.appendChild(newTr);
-    newTr.appendChild(thEl);  //Append table data to table row
-    thEl.textContent = 'Cups Sold';
-
-
-    for(var i = 0; i < location.customerPerHrArray.length; i++) {  //Loop to create table headings for the hours
-
-      var tdEl = document.createElement('td');
-      tdEl.textContent = Math.ceil(location.numCupsSoldArray[i]);
-      newTr.appendChild(tdEl);
-    }
-
-
-}
-var allKiosks = [pikePlace, capitolHill, seaPubLib, sLakeUnion, seaTacAirport, websiteSales];
-
-function renderTables() {
-  for (var i=0; i<allKiosks.length; i++){
-    renderTable(allKiosks[i]);
-  }
-}
-
-renderTables();
-
-var newKiosk = document.getElementById('newKiosk');
-newKiosk.addEventListener('submit', addNewKiosk);
-
-function addNewKiosk(event) {
-  console.log(event);
-  event.preventDefault();
-
-  var storeName = event.target.storeName.value;
-  var minCust = event.target.minCust.value;
-  var maxCust = event.target.maxCust.value;
-  var avgCups = event.target.avgCups.value;
-  var avgPnds = event.target.avgPnds.value;
-
-  var eventKiosk = new Kiosk(storeName, minCust, maxCust, avgCups, avgPnds);
-  renderTable(eventKiosk);
-}
-
-
-//Aggregate Kiosk Sales
-// var aggregateKioskSales = 0; //Initialize to zero
-
-// var kiosks = [this.pikePlace, this.capitolHill, this.seaPubLib, this.sLakeUnion, this.seaTacAirport, this.websiteSales]; //Array of kiosk objects
-// for (var i = 0; i < kiosks.length; i++) {
-//   kiosks[i].render();
-//   aggregateKioskSales += kiosks[i].totalDailyLbs;
+//   var ulEl = document.createElement('ul');
+//   ulEl.appendChild(document.createTextNode(this.name));
+//   for(var i = 0; i < hours.length; i++) {
+//     var liEl = document.createElement('li');
+//     //6:00am: 86.5 lbs [23 customers, 27.6 cups (1.4 lbs), 85.1 lbs to-go]
+//     liEl.textContent = hours[i] + ': ' + this.totalLbsSoldPerHrArray[i].toFixed(1) + ' lbs [' + this.customerPerHrArray[i] + ' customers, ' + this.numCupsSoldArray[i].toFixed(1) + ' cups (' + this.cupsToLbsArray[i].toFixed(1) + ' lbs), ' + this.toGoLbsArray[i].toFixed(1) + ' lbs to-go]';
+//     ulEl.appendChild(liEl);
+//   }
+//   document.body.appendChild(ulEl); //This takes the <ul> element and puts it in the body as child element
+//   liEl = document.createElement('li');
+//   liEl.textContent = 'TOTAL Daily Pounds At ' + this.name + ': ' + this.totalDailyLbs.toFixed(1) + ' lbs';
+//   ulEl.appendChild(liEl);
 // }
-// var h1El = document.createElement('h1');
-// h1El.textContent = 'Total daily pounds of coffee for all locations is: ' + aggregateKioskSales.toFixed(1) + ' lbs.';
-// document.body.appendChild(h1El);
